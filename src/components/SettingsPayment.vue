f3e9c776f9073f59a23d4fb045041ebb564778f7
kum83 <momo821123@outlook.com>
2021-08-29 16:12:18 +0300

    f

--- src/components/SettingsPayment.vue
+++ src/components/SettingsPayment.vue
@@ -8,41 +8,46 @@
 
       <loading-animation v-if="loading"/>
 
-      <div v-else class="flex--50">
-
-        <div>
-          <md-checkbox v-model="form.payment_show_visa_mastercard_logos">{{
-              $t('global.ShowVisaMastercardLogos')
-            }}
-          </md-checkbox>
+      <div v-else>
+        <div v-if="form.plan == ''">
+          <SettingsPaymentSelectPlan></SettingsPaymentSelectPlan>
         </div>
+        <div v-else class="flex--50">
 
-        <div>
-          <md-checkbox v-model="form.payment_show_dankort_logo">{{ $t('global.ShowDankortLogo') }}</md-checkbox>
+          <div>
+            <md-checkbox v-model="form.payment_show_visa_mastercard_logos">{{
+                $t('global.ShowVisaMastercardLogos')
+              }}
+            </md-checkbox>
+          </div>
+
+          <div>
+            <md-checkbox v-model="form.payment_show_dankort_logo">{{ $t('global.ShowDankortLogo') }}</md-checkbox>
+          </div>
+
+          <div>
+            <md-checkbox v-model="form.payment_show_mobilepay_logo">{{ $t('global.ShowMobilepayLogo') }}</md-checkbox>
+          </div>
+
+          <md-field>
+            <label>{{ $t('global.ReepayAPIkey') }}</label>
+            <md-input type="text" v-model="form.payment_service_provider_reepay_private_api_key" required></md-input>
+          
+          </md-field>
+
+          <md-field>
+            <label>{{ $t('global.ReepayWebhookSecret') }}</label>
+            <md-input type="text" v-model="form.payment_service_provider_reepay_webhook_secret" required></md-input>
+          
+          </md-field>
+
+          <div class="mt2">
+            <md-button class="md-raised md-primary" @click="submit">{{ $t('global.Save') }}</md-button>
+          </div>
         </div>
-
-        <div>
-          <md-checkbox v-model="form.payment_show_mobilepay_logo">{{ $t('global.ShowMobilepayLogo') }}</md-checkbox>
-        </div>
-
-        <md-field>
-          <label>{{ $t('global.ReepayAPIkey') }}</label>
-          <md-input type="text" v-model="form.payment_service_provider_reepay_private_api_key" required></md-input>
-         
-        </md-field>
-
-        <md-field>
-          <label>{{ $t('global.ReepayWebhookSecret') }}</label>
-          <md-input type="text" v-model="form.payment_service_provider_reepay_webhook_secret" required></md-input>
-         
-        </md-field>
-
 
       </div>
 
-      <div class="mt2">
-        <md-button class="md-raised md-primary" @click="submit">{{ $t('global.Save') }}</md-button>
-      </div>
 
       <div class="space8"></div>
       <div class="space8"></div>
@@ -60,11 +65,14 @@ import YogoApi from '@/gateways/YogoApi';
 import { mapGetters } from 'vuex';
 import _pick from 'lodash/pick';
 
+import SettingsPaymentSelectPlan from './SettingsPaymentSelectPlan';
+
 export default {
 
   components: {
     SettingsSubMenu,
     LoadingAnimation,
+    SettingsPaymentSelectPlan,
   },
 
   data() {
@@ -85,10 +93,12 @@ export default {
         '&keys[]=payment_show_dankort_logo' +
         '&keys[]=payment_show_mobilepay_logo' +
         '&keys[]=payment_service_provider_reepay_webhook_secret' +
-        '&keys[]=payment_service_provider_reepay_private_api_key',
-        
-        
+        '&keys[]=payment_service_provider_reepay_private_api_key' +
+        '&keys[]=plan' +
+        '&keys[]=payment_service_provider',
     );
+    console.log(" == plan = #", this.form.plan + "#")
+    console.log(" == payment_service_provider = ", this.form.payment_service_provider)
     this.loading = false;
   },
   methods: {
---
+++ src/components/SettingsPaymentSelectPlan.vue
@@ -0,0 +1,151 @@
+<template>
+    <div>
+        <h1 class="mb-8">{{ $t('payment.PleaseSelectYogoPlan') }}</h1>
+        {{ $t('payment.SelectPlanPleaseChoose') }}
+
+        <div class="flex flex-wrap mb-4">
+            <div class="yogo-plan-card flex--25 m-4">
+                <div class="plan-first-row">{{ $t('payment.PayAsYouGrow') }}</div>
+                <div class="plan-second-row">5%</div>
+                <div class="plan-third-row">{{$t('payment.SelectPlanOfYourSales') }}</div>
+            </div>
+
+            <div class="yogo-plan-card flex--25 m-4">
+                <div class="plan-first-row">{{ $t('payment.StudioLight') }}</div>
+                <div class="plan-second-row">499,-</div>
+                <div class="plan-third-row">DKK / {{$t('payment.Month') }}</div>
+            </div>
+
+            <div class="yogo-plan-card flex--25 m-4">
+                <div class="plan-first-row">{{ $t('payment.Studio') }}</div>
+                <div class="plan-second-row">999,-</div>
+                <div class="plan-third-row">DKK / {{$t('payment.Month') }}</div>
+            </div>
+
+            <div class="yogo-plan-card flex--25 m-4">
+                <div class="plan-first-row">{{ $t('payment.StudioApp') }}</div>
+                <div class="plan-second-row">1999,-</div>
+                <div class="plan-third-row">DKK / {{$t('payment.Month') }}</div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'SettingsPaymentSelectPlan',
+
+    data() {
+        return {
+            
+        };
+    }
+};
+
+</script>
+
+<style lang="scss" scoped>
+    // .yogo-plan-card:hover {
+    //     cursor: pointer;
+    // }
+
+    .yogo-plan-card {
+        border: 1px solid black;
+        background-color: #CFE2F3;
+        max-width: 180px;
+
+        &:hover {
+            cursor: pointer;
+        }
+    }
+    .plan-first-row {
+        font-size: 16px;
+        text-align: center;
+        margin-top: 10px;
+    }
+    .plan-second-row {
+        font-size: 40px;
+        text-align: center;
+        margin: 30px auto
+    }
+    .plan-third-row {
+        font-size: 12px;
+        text-align: center;
+        margin-bottom: 30px;
+    }
+
+// .pagination-top-container {
+//   padding-bottom: 5px;
+//   background: rgb(236, 239, 241);
+// }
+
+// .pagination-bottom-container {
+//   padding-top: 5px;
+//   background: rgb(236, 239, 241);
+// }
+
+// .yogo-table-content {
+//   overflow-x:scroll;
+// }
+
+// table {
+//   width: 100%;
+//   border-spacing: 0;
+//   border-collapse: collapse;
+//   overflow: hidden;
+
+//   th {
+//     text-align: left;
+//     padding: 0;
+//     position: relative;
+//     font-size: 12px;
+//     line-height: 16px;
+//     color: rgba(0, 0, 0, 0.54);
+
+//     &.numeric {
+//       text-align: right;
+//     }
+
+//     .table-sort-arrow {
+//       opacity: 0;
+//       width: 16px;
+//       height: 16px;
+//       transition: .3s cubic-bezier(.4, 0, .2, 1);
+//     }
+
+//     &.sortable {
+//       cursor: pointer;
+//     }
+
+//     &.sortable:hover .table-sort-arrow,
+//     &.sorted .table-sort-arrow {
+//       opacity: 1;
+//     }
+
+//     &.sorted-desc .table-sort-arrow {
+//       transform: rotate(180deg);
+//     }
+//   }
+
+
+//   td {
+//     height: 48px;
+//     position: relative;
+//     transition: .3s cubic-bezier(.4, 0, .2, 1);
+//     font-size: 13px;
+//     line-height: 18px;
+//   }
+
+//   tr.md-has-selection:hover {
+
+//     td {
+//       background: rgba(0, 0, 0, 0.08);
+//     }
+
+//   }
+
+// }
+
+</style>
